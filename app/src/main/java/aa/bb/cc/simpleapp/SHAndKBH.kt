02cf7f222db50babeefee0cc1f821363f382e460
waiferkolar <72coder@gmail.com>
2020-03-15 17:34:25 +0630

    KB Hide

--- app/src/main/java/aa/bb/cc/simpleapp/SHAndKBH.kt
+++ app/src/main/java/aa/bb/cc/simpleapp/SHAndKBH.kt
@@ -1,18 +1,33 @@
 package aa.bb.cc.simpleapp
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.view.inputmethod.InputMethodManager
+import androidx.annotation.RequiresApi
 import kotlinx.android.synthetic.main.activity_shand_kbh.*
+import org.jetbrains.anko.contentView
 import org.jetbrains.anko.toast
 
 class SHAndKBH : AppCompatActivity() {
 
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shand_kbh)
+
+        /*
+        etUseranme.setOnFocusChangeListener { view, b -> hideKB() }
+        etPassword.setOnFocusChangeListener { view, b -> hideKB() }
+         */
+
+        etUseranme.showSoftInputOnFocus = false
+        etPassword.showSoftInputOnFocus = false
 
         btnLogin.setOnClickListener {
             val username = etUseranme.text.toString()
@@ -31,6 +46,13 @@ class SHAndKBH : AppCompatActivity() {
                 toast("Please fill all required fields!")
             }
         }
+    }
+
+
+    private fun hideKB(){
+        val imm =  getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(contentView?.windowToken,0)
+
     }
 
     private fun wayTwo() {
